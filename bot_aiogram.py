14bbd231b028491c9c3a09ba699ee00dc3eedc55
OSAndreev <andreevos22@mail.ru>
2023-11-18 21:17:19 +0300

    added delay

--- bot_aiogram.py
+++ bot_aiogram.py
@@ -3,12 +3,14 @@ from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
 from handlers import common
+import time
 
 
 
 
 
 async def main():
+    time.sleep(4)
     bot = Bot(token="6937186847:AAHGhMg2pPSvcAOsISPu8nun_8pyXWBNNV8")
     dp = Dispatcher()
     dp.include_router(common.router)
